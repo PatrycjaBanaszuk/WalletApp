c9bb6f4b678b63f88e84a412ddcdcccc1f7aa691
Patrycja <pwbanaszuk@gmail.com>
2022-04-01 21:02:57 +0100

    added style to transaction panel

---
+++ script.js
@@ -0,0 +1,10 @@
+const incomeSection = document.querySelector('.income-area')
+const expensesSection = document.querySelector('.expenses-area')
+const availableMoney = document.querySelector('available-money')
+const addTransactionalPanel = document.querySelector('.add-transaction-panel')
+
+const nameInput = document.querySelector('#name')
+const amountInput = document.querySelector('#amount')
+const categorySelect = document.querySelector('#category')
+
+const addTransactionalPanel = document.querySelector('.add-transaction-panel')
