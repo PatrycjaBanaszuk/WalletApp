47f8cc5fb585eaeb2ceb217c0fa45b1573ad422d
Patrycja <pwbanaszuk@gmail.com>
2022-04-05 16:09:42 +0100

    fixed small errors

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-const incomeSection = document.querySelector('.income-area')
-const expensesSection = document.querySelector('.expenses-area')
-const availableMoney = document.querySelector('available-money')
-const addTransactionalPanel = document.querySelector('.add-transaction-panel')
+const incomeSection = document.querySelector('.income-area');
+const expensesSection = document.querySelector('.expenses-area');
+const availableMoney = document.querySelector('.available-money');
+const addTransactionPanel = document.querySelector('.add-transaction-panel');
 
-const nameInput = document.querySelector('#name')
-const amountInput = document.querySelector('#amount')
-const categorySelect = document.querySelector('#category')
+const nameInput = document.querySelector('#name');
+const amountInput = document.querySelector('#amount');
+const categorySelect = document.querySelector('#category');
 
-const addTransactionalBtn = document.querySelector('.add-transaction')
-const saveBtn = document.querySelector('.save')
-const cancelBtn = document.querySelector('.cancel')
-const deleteBtn = document.querySelector('.delete')
-const deleteAllBtn = document.querySelector('.delete-all')
+const addTransactionBtn = document.querySelector('.add-transaction');
+const saveBtn = document.querySelector('.save');
+const cancelBtn = document.querySelector('.cancel');
+const deleteBtn = document.querySelector('.delete');
+const deleteAllBtn = document.querySelector('.delete-all');
 
 let root = document.documentElement
 let ID = 0
-let categoryIcon;
-let selectedCategory;
+let categoryIcon
+let selectedCategory
 let moneyArr = [0]
+
+const showPanel = () => {
+	console.log('fnkfjdf');
+	addTransactionPanel.style.display = 'flex';
+}
+
+const closePanel = () => {
+	addTransactionPanel.style.display = 'none';
+}
+
+addTransactionBtn.addEventListener = ('click', showPanel);
+cancelBtn.addEventListener = ('click', closePanel);
