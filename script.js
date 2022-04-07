d2782925f7daf7fbe18e2c705df6807bbd9e3a1d
Patrycja <pwbanaszuk@gmail.com>
2022-04-07 14:41:13 +0100

    added function checkform

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const incomeSection = document.querySelector('.income-area');
-const expensesSection = document.querySelector('.expenses-area');
-const availableMoney = document.querySelector('.available-money');
-const addTransactionPanel = document.querySelector('.add-transaction-panel');
+const incomeSection = document.querySelector('.income-area')
+const expensesSection = document.querySelector('.expenses-area')
+const availableMoney = document.querySelector('.available-money')
+const addTransactionPanel = document.querySelector('.add-transaction-panel')
 
-const nameInput = document.querySelector('#name');
-const amountInput = document.querySelector('#amount');
-const categorySelect = document.querySelector('#category');
+const nameInput = document.querySelector('#name')
+const amountInput = document.querySelector('#amount')
+const categorySelect = document.querySelector('#category')
 
-const addTransactionBtn = document.querySelector('.add-transaction');
-const saveBtn = document.querySelector('.save');
-const cancelBtn = document.querySelector('.cancel');
-const deleteBtn = document.querySelector('.delete');
-const deleteAllBtn = document.querySelector('.delete-all');
+const addTransactionBtn = document.querySelector('.add-transaction')
+const saveBtn = document.querySelector('.save')
+const cancelBtn = document.querySelector('.cancel')
+const deleteBtn = document.querySelector('.delete')
+const deleteAllBtn = document.querySelector('.delete-all')
 
 let root = document.documentElement
 let ID = 0
@@ -20,13 +20,22 @@ let selectedCategory
 let moneyArr = [0]
 
 const showPanel = () => {
-	console.log('fnkfjdf');
-	addTransactionPanel.style.display = 'flex';
+	addTransactionPanel.style.display = 'flex'
 }
-
 const closePanel = () => {
-	addTransactionPanel.style.display = 'none';
+	addTransactionPanel.style.display = 'none'
 }
 
-addTransactionBtn.addEventListener = ('click', showPanel);
-cancelBtn.addEventListener = ('click', closePanel);
+const checkForm = () => {
+    if(nameInput.value !== '' && amountInput.value !== ''&& categorySelect.value !== 'none'){
+        console.log(ok);
+    } else {
+        alert('Fill in all the fields!')
+    }
+}
+
+
+addTransactionBtn.addEventListener('click', showPanel)
+cancelBtn.addEventListener('click', closePanel)
+saveBtn.addEventListener('click', checkForm)
+
